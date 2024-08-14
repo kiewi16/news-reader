d9920562a7e05b203685ecda3e7602eb54d8c2fd
Kim Ewing <pekar.kimberly@gmail.com>
2024-08-14 12:00:42 -0600

    revises endpoint so the 30 articles returned are the most relevant to the keyword science instead of the 30 most recent articles.

--- src/apiCall.js
+++ src/apiCall.js
@@ -4,7 +4,7 @@
 // }
 
 function getArticles() {
-    return fetch("https://newsapi.org/v2/everything?q=science&sortBy=publishedAt&language=en&pageSize=30&apiKey=61c3d32aeb4943d0a55e840f01a6e449")
+    return fetch("https://newsapi.org/v2/everything?q=science&sortBy=relevancy&language=en&pageSize=30&apiKey=61c3d32aeb4943d0a55e840f01a6e449")
     .then(response => {
         if(!response.ok) {
             throw new Error("We've encountered an unexpected error and were unable to get the articles/")
